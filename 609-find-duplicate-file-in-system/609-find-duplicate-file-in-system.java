67cd94d510b4f6c820b600810df78e9acb4eb3bc
Deepak Kumar <99772696+deepak2002139@users.noreply.github.com>
2022-09-19 19:34:36 +0530

    Time: 31 ms (92.14%), Space: 51.7 MB (89.17%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.java
@@ -0,0 +1,45 @@
+// class Solution
+// {
+//     public List<List<String>>findDuplicate(String[]paths)
+//     {
+//         HashMap<String,List<String>> map= new HashMap<>();
+//         for(String path:paths)
+//         {
+//             String [] arr=path.split(" ");
+//             String directory =arr[0];
+
+//             for(int i=1;i<arr.length;i++)
+//             {
+//                 String fileName = arr[i].substring(0,arr[i].indexOf("("));
+//                 Sring Content =arr[i].substring(arr[i].indexOf("(")+1,arr[i].length()-1);
+//                 List<String> filepaths=map.getOrDefault(Content,new ArrayList<>());
+//                 filepaths.add(directory+"/"+fileName);
+//                 store.put(content,filepaths);
+//             }
+//         }
+//         map.entrySet().removeIf(entry->entry.getvalue().size()>2);
+//     }
+// }
+class Solution {
+    public List<List<String>> findDuplicate(String[] paths) {
+        Map<String,List<String>> store = new HashMap<>();
+        
+        for(String path:paths){
+            String[] arr = path.split(" ");
+            String directory = arr[0];
+            
+            for(int i=1;i<arr.length;i++){
+                //fileName(content)
+                String fileName = arr[i].substring(0,arr[i].indexOf("("));
+                String content = arr[i].substring(arr[i].indexOf("(")+1,arr[i].length()-1);
+                List<String> filepaths = store.getOrDefault(content,new ArrayList<>());
+                filepaths.add(directory +"/"+fileName);
+                store.put(content,filepaths);
+            }
+        }
+        
+        store.entrySet().removeIf(entry -> entry.getValue().size()<2);
+        
+        return new ArrayList<>(store.values());
+    }
+}
