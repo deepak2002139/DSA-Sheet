97f40ecd032eed9cc7eb59facbb7dd0e4710dc2c
Deepak Kumar <99772696+deepak2002139@users.noreply.github.com>
2022-10-03 13:53:59 +0530

    Time: 75 ms (45.00%), Space: 19.1 MB (69.32%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,52 +1,56 @@
-// class Solution {
-// public:
-//     vector<vector<int>> merge(vector<vector<int>>& intervals) 
-//     {
-//         if(intervals.size()<2)
-//             return intervals;
-//         int i=1;
-//         vector<vector<int>> ans;
-//         sort(intervals.begin(),intervals.end());
-//         ans.push_back(intervals[0]);
-//         int n =intervals.size();
-//         int m= intervals.size();
-//         while(i<intervals.size())
-//         {
-//             if(intervals[i][0]<=ans.back()[1])
-//             {
-//                 if(ans.back()[1]<=intervals[i][1])
-//                 {
-//                     ans.back()[1]=intervals[i][1];
-//                 }
-//                 else{ans.push_back(intervals[i]);
-//                     }
-//                 i++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-class Solution {
+class Solution 
+{
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
         if(intervals.size()<2)
-        return intervals;
+            return intervals;
         int i=1;
         vector<vector<int>> ans;
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(),intervals.end());
         ans.push_back(intervals[0]);
+        // int n =intervals.size();
+        // int m= intervals.size();
         while(i<intervals.size())
         {
-            if(intervals[i][0]<=ans.back()[1]){
-                if(ans.back()[1]<=intervals[i][1]){
+            if(intervals[i][0]<=ans.back()[1])
+            {
+                if(ans.back()[1]<=intervals[i][1])
+                {
                     ans.back()[1]=intervals[i][1];
                 }
             }
-            else{
-                ans.push_back(intervals[i]);
-            }
+            else
+                {
+                    ans.push_back(intervals[i]);
+                }
+ 
             i++;
         }
         return ans;
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         if(intervals.size()<2)
+//         return intervals;
+//         int i=1;
+//         vector<vector<int>> ans;
+//         sort(intervals.begin(), intervals.end());
+//         ans.push_back(intervals[0]);
+//         while(i<intervals.size())
+//         {
+//             if(intervals[i][0]<=ans.back()[1]){
+//                 if(ans.back()[1]<=intervals[i][1]){
+//                     ans.back()[1]=intervals[i][1];
+//                 }
+//             }
+//             else{
+//              s   ans.push_back(intervals[i]);
+//             }
+//             i++;
+//         }
+//         return ans;
+//     }
+// };
