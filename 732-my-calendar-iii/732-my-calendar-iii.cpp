b2390ff61caead6b59d1b39ea553b907d9906afa
Deepak Kumar <99772696+deepak2002139@users.noreply.github.com>
2022-10-07 14:21:13 +0530

    Time: 283 ms (24.40%), Space: 26.4 MB (37.06%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,42 +1,18 @@
-class MyCalendarThree 
-{
-    map<pair<int,int>,int >map1 ;
+class MyCalendarThree {
+    map<int, int> mp;
 public:
-    MyCalendarThree() 
-    {
-        map1=map<pair<int,int>,int>(); 
+    MyCalendarThree() {
         
     }
     
-    int book(int start, int end) 
-    {
-        map<pair <int , int >, int >map2;
-        int res =1;
-        for(const auto& p : map1)
-        {
-            int newStart =max(start, p.first.first);
-            int newEnd = min(end, p.first.second);
-            if(newStart< newEnd)
-            {
-                map2[{newStart,newEnd}]= max(map2[{newStart, newEnd}],p.second+1);
-                res=max(map2[{newStart, newEnd}],res);
-            }
-            else{
-                res=max(res,p.second);
-            }                               
-        }
-        for(const auto& p :map2) 
-        {
-            map1[p.first] = p.second;
-        }
-        if (map1.find({start, end}) == map1.end()) 
-            map1[{start, end}] = 1;
-        return res;
+    int book(int start, int end) {
+        
+        mp[start]++, mp[end]--;
+        int ans = 0, curr = 0;
+        
+        for(auto &i : mp)
+            curr += i.second, ans = max(ans, curr);
+        
+        return ans;
     }
 };
-
-/**
- * Your MyCalendarThree object will be instantiated and called as such:
- * MyCalendarThree* obj = new MyCalendarThree();
- * int param_1 = obj->book(start,end);
- */
