bedf4f4ab89cd4e06d6d09028511e340d72268fb
Deepak Kumar <99772696+deepak2002139@users.noreply.github.com>
2022-10-28 22:59:57 +0530

    Time: 50 ms (89.58%), Space: 9.4 MB (77.78%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,5 +1,26 @@
-class Solution {
+class Solution 
+{
+//     brute force algorithm 
 public:
+//     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+//         int n = img1.size();
+//         int cnt[60][60]={0};
+//         int ans = 0;
+//         for(int i = 0;i<n;i++)
+//             for(int j = 0;j<n;j++){
+//                 if(img1[i][j]==0) continue;
+//                 for(int I = 0;I<n;I++){
+//                     for(int J = 0;J<n;J++){
+//                         if(img2[I][J]==0) continue;
+//                         ans = max(ans,++cnt[n+i-I][n+j-J]);
+//                     }
+//                 }
+//             }
+
+//         return ans;
+//     
+//     
+//     using map this soluion is been solved 
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
         int cnt[60][60]={0};
@@ -17,4 +38,5 @@ public:
 
         return ans;
     }
+    
 };
