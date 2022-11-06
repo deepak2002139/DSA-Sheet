ba15f4145e078319ffbac9f7eb20cc5173224920
Deepak Kumar <99772696+deepak2002139@users.noreply.github.com>
2022-11-07 01:45:30 +0530

    Time: 4 ms (84.00%), Space: 8.3 MB (75.43%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.cpp
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    
-    //very easy problem
-    string orderlyQueue(string s, int k) {
-        
-        //just have to calculate for k==1, 
-		//when s = "cba", --> cbacba
+    string orderlyQueue(string s, int k) 
+    {
         if(k==1)
         {
-            int n=s.length();
-            string doublestring = s+s;      //append same string to s
-            string ans = s;
-            
-            for(int i=1; i<n; i++)
+            int n =s.size();
+            string doublestring=s+s;
+            string ans=s;
+            for(int i=1;i<n;i++)
             {
                 string str = doublestring.substr(i,n);
-                
-                if(str < ans)
+                if(str<ans)
                 {
                     ans = str;
                 }
             }
             return ans;
         }
-        
-        sort(s.begin(), s.end());
+        sort(s.begin(),s.end());
         return s;
+        
     }
 };
