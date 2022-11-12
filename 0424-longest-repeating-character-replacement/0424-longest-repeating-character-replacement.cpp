47b5cf702902c99b660e0303c7e6fe3cd9e9b5c1
Deepak Kumar <99772696+deepak2002139@users.noreply.github.com>
2022-11-12 12:55:14 +0530

    Time: 39 ms (20.76%), Space: 7.3 MB (7.72%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,46 +1,31 @@
-class Solution 
-{
+class Solution {
 public:
     int characterReplacement(string s, int k)
     {
-            unordered_set<char>st;
-            for(auto &e : s)
+        int  n = s.size();
+        int ans =0;
+        int i =0 , j =0, maxi =0;
+        unordered_map<char , int> mp ;
+        while(j<n)
+        {
+            while(j<n)
             {
-                st.insert(e);
+            mp[s[j]]++;
+            maxi= max(mp[s[j]],maxi);
+                if(mp.size()>1 && j-i+1-maxi>k)
+                    break;
+                ans= max(ans,j-i+1);
+                j++;
             }
-            int ans = 0;
-            for(auto &e : st)
-            {
-                int i = 0;
-                int j = 0;
-                int cnt = 0;
-                int currLen = 0;
-                while(j < s.length())
-                {
-                    if(s[j] == e)
-                    {
-                        currLen++;
-                        j++;
-                        ans = max(currLen, ans);
-                    }
-                    else
-                    {
-                        cnt++;
-                        currLen++;
-                        while(cnt > k && i <= j)
-                        {
-                            if(s[i] != e)
-                            {
-                                cnt--;
-                            }
-                            i++;
-                            currLen--;
-                        }
-                        ans = max(currLen, ans);
-                        j++;
-                    }
-                }
-            }
-            return ans;
-		}
+            if(maxi==mp[s[i]])
+                maxi--;
+            mp[s[i]]--;
+            if(mp[s[i]]==0)
+                mp.erase(s[i]);
+            i++;
+            j++;
+            
+        }
+        return ans;
+    }
 };
