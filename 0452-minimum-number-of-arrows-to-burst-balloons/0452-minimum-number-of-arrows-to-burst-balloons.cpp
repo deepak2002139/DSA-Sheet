8d85935bca2a8ad4eb4a948199be5ceaaf091063
Deepak Kumar <99772696+deepak2002139@users.noreply.github.com>
2023-01-05 12:21:34 +0530

    Time: 1111 ms (21.75%), Space: 89.8 MB (68.04%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,27 +1,34 @@
-bool com (vector<int>& l, vector<int>& r) {
+bool com (vector<int>& l, vector<int>& r) 
+{
     return l[0] < r[0];
 }
-class Solution {
+class Solution
+{
 public:
-    int findMinArrowShots(vector<vector<int>>& points) {
+    int findMinArrowShots(vector<vector<int>>& points)
+    {
         std::sort(points.begin(), points.end(), com);
         int i = 0;
         int len = points.size();
         int cxs, cxe, dxs, dxe;
         int res = 0;
-        while (i < len) {
-            cxs = points[i][0];
-            cxe = points[i][1];
+        while (i<len)
+        {
+            cxs=points[i][0];
+            cxe=points[i][1];
             i++;
-            while (i < len) {
+            while (i < len)
+            {
                 dxs = points[i][0];
                 dxe = points[i][1];
-                if (cxe >= dxs && dxe >= cxs) {
-                    cxs = max(cxs, dxs);
-                    cxe = min(cxe, dxe);
+                if(cxe >= dxs && dxe >= cxs) 
+                {
+                    cxs=max(cxs, dxs);
+                    cxe=min(cxe, dxe);
                     i++;
                 }
-                else {
+                else 
+                {
                     break;
                 }
             }
