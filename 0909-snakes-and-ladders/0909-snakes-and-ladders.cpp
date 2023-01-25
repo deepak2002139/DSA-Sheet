fde483cc5999861e83b5f7b0466bbd3d99940f8a
Deepak Kumar <99772696+deepak2002139@users.noreply.github.com>
2023-01-25 06:00:38 +0530

    Time: 276 ms (5.69%), Space: 12.7 MB (78.06%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -56,59 +56,29 @@
 // };
 class Solution {
 public:
-    int dijkstra(int src, int tar, vector<int>& a)
-    {
-        vector<int>dis(tar + 2, 1e5);
-        priority_queue<pair<int, int>, vector< pair<int, int>>, greater< pair<int, int>>>pq;
-        dis[src] = 0;
-        pq.push({ 0,src });
-        while (!pq.empty())
-        {
-            int u = pq.top().second;
-            int c = pq.top().first;
-            pq.pop();
-            if (u == tar)
-            {
-                return c;
-            }
-            
-            for (int i = 1; i <= 6; i++)
-            {
-                if (i + u > tar)
-                    break;
-                int v;
-                if (a[i + u] != -1)
-                    v = a[i + u];
-                else
-                    v = i + u;
-                if (dis[v] > dis[u] + 1)
-                {
-                    dis[v] = dis[u] + 1;
-                    pq.push({ dis[v],v });
-                }
-            }
-        }
-        return -1;
-    }
     int snakesAndLadders(vector<vector<int>>& board) {
-        reverse(board.begin(), board.end());
-        vector<int>a;
-        a.push_back(0);
-        int n = board.size();
-        n *= n;
-        int m = 0;
-        for (auto i : board)
-        {
-            if (m & 1)
-                reverse(i.begin(), i.end());
-            for (auto j : i)
-                a.push_back(j);
-            m++;
+        int N = board.size();
+        vector<int> list(N * N);
+        vector<int> mins(N * N, -1);
+        int i, j;
+        for(i = 0; i < N; ++i) {
+            for(j = 0; j < N; ++j) {
+                if(i % 2 == 0) list[i * N + j] = board[N - 1 - i][j] - 1;
+                else list[i * N + N - j - 1] = board[N - 1 - i][j] - 1;
+            }
         }
-
-        int ret = dijkstra(1, n, a);
-        cout << ret << "\n";
-        
-        return ret;
+        recur(list, mins, 0, 0, N * N - 1);
+        return mins[N * N - 1];
+    }
+    
+    void recur(vector<int> &board, vector<int> &mins, int cur, int cur_s, int M) {
+        if(mins[cur] != -1 && mins[cur] <= cur_s) return; 
+        if(mins[cur] == -1 || cur_s < mins[cur]) mins[cur] = cur_s;
+        if(cur == M) return;
+        for(int i = cur + 1; i <= cur + 6 && i <= M; ++i) {
+            if(board[i] >= 0) recur(board, mins, board[i], cur_s + 1, M);
+            else recur(board, mins, i, cur_s + 1, M);
+        }
+        return;
     }
 };
